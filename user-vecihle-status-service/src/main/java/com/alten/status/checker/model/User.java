f5338ba1183a2bd773654b6771628d8b4fd98c88
Mete <bmcnpnr@users.noreply.github.com>
2019-06-06 22:20:02 +0300

    finished creating relational database

--- user-vecihle-status-service/src/main/java/com/alten/status/checker/model/User.java
+++ user-vecihle-status-service/src/main/java/com/alten/status/checker/model/User.java
@@ -1,7 +1,69 @@
 package com.alten.status.checker.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "USER")
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "USER_ID")
+    private int id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "SURNAME")
+    private String surname;
+    @Column(name = "ADDRESS")
+    private String address;
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
 }
--- user-vecihle-status-service/src/main/java/com/alten/status/checker/model/Vehicle.java
+++ user-vecihle-status-service/src/main/java/com/alten/status/checker/model/Vehicle.java
@@ -1,7 +1,43 @@
 package com.alten.status.checker.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "VEHICLE")
 public class Vehicle {
+    @Id
+    @Column(name = "VEHICLE_ID")
+    private String vehicleId;
+    @Column(name = "REGISTRATION_NUMBER")
+    private String registrationNumber;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public Vehicle() {
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
