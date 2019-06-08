b1443f75c81183db367d0a2c9923e8204c1bb933
Mete <bmcnpnr@users.noreply.github.com>
2019-06-08 12:50:03 +0300

    connection between the micro services established via gateway

--- user-vecihle-status-service/src/main/java/com/alten/status/checker/service/StatusChecker.java
+++ user-vecihle-status-service/src/main/java/com/alten/status/checker/service/StatusChecker.java
@@ -1,7 +1,11 @@
 package com.alten.status.checker.service;
 
 import com.alten.status.checker.model.dto.UserVehicleStatus;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +23,13 @@ public class StatusChecker {
     private RestTemplate restTemplate;
 
     public List<UserVehicleStatus> getVehicleStatus() {
+
+        String fooResourceUrl = "http://localhost:8762/statusSimulator";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl + "/getVehicleStatus", String.class);
+        JsonElement jsonElement = new Gson().toJsonTree(response.getBody());
+        System.out.println(jsonElement.getAsString());
+
         return null;
     }
 }
