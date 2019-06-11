30efac6d042755d617b9cdfb6769e6469bb0ecdb
Burak Mete Canpınar <burakmetecanpinar@gmail.com>
2019-06-11 09:37:18 +0300

    Created script to run it locally

---
+++ build/docker/scripts/run.sh
@@ -0,0 +1,25 @@
+#Building every service and creating docker images
+
+cd ../../../service-registration-server
+
+mvn clean install
+
+cd ../api-gateway
+
+mvn clean install
+
+cd ../user-vehicle-status-service
+
+mvn clean install
+
+cd ../status-simulator
+
+mvn clean install
+
+# Enter docker-compose folder
+
+cd ../build/docker/
+
+# Deploy
+
+docker-compose up --build -d
